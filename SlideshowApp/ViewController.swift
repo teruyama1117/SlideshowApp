43cef9264147e00ce6f8ccb6f8e6ba93b388858c
teruteru <teruterubouzu@techacademy.jp>
2023-03-14 22:51:29 +0900

    Initial Commit

---
+++ SlideshowApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SlideshowApp
+//
+//  Created by Teruo Yamamoro on 2023/03/14.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
